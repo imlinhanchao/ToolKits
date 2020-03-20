9335c1100c1cb1751b0845cb1db324a6d81421ce
Hancel.Lin <imlinhanchao@gmail.com>
2020-03-20 23:29:24 +0800

    声明右键菜单结构体

--- ToolKit/library/ContextMenu.h
+++ ToolKit/library/ContextMenu.h
@@ -3,13 +3,32 @@
 
 namespace Lib {
 
+typedef struct _CONTEXTMENU{
+	enum TYPE { UNKNOWN, FILE, FOLDER, CONTEXT };
+
+	CString sName;
+	CString sDesc;
+	CString sIcon;
+
+	CString sProgramRegistryKey;
+	CString sRegistryKey;
+	CString sProgram;
+	CString sCommand;
+
+	vector<CString> sExtensions;
+	bool bEnable;
+	TYPE eType;
+
+} CONTEXTMENU, *PCONTEXTMENU;
+
 class CContextMenu : public Base
 {
 public:
 	CContextMenu(void);
 	~CContextMenu(void);
 
-
+private:
+	vector<CONTEXTMENU> m_menus;
 };
 
 }
